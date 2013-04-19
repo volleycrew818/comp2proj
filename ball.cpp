434b13078329f6ae23b52a5257d84567529ed9e4
volleycrew818 <kkiely@nd.edu>
2013-04-19 07:36:08 -0300

    Update ball.cpp

--- ball.cpp
+++ ball.cpp
@@ -2,9 +2,21 @@
 #include <SDL/SDL.h>
 #include <SDL/SDL_image.h>
 #include <iostream>
+#include "SDL/SDL_mixer.h"
 using namespace std;
 
+//The sound effect
+Mix_Chunk *bounce = NULL;
+
 Ball::Ball(){
+    //Load sound effects
+    bounce = Mix_LoadWAV( "slap".wav" );
+     //If there was a problem loading the sound effects
+    if( bounce == NULL  )
+    {
+        cout<< "Problem with loading sound"
+        return 1;
+    }
     x = 0;
     y = 0;
     xv = 3, yv = 5;
@@ -12,27 +24,54 @@ Ball::Ball(){
     if (loaded == NULL) { cout << "well shit"<<endl; exit(1);}
     image = SDL_DisplayFormat(loaded);
     if (image == NULL) cout<< "double shit" << endl;
+    //Freesound effects
+    Mix_FreeChunk( bounce );
 }
 
 void Ball::move() {
     x += xv;
     y += yv;
         //left wall and start area
-    if (x>460 || x<0) xv = -xv;
+    if (x>460 || x<0) 
+    {
+        //Play effect
+         if( Mix_PlayChannel( -1, bounce, 0 ) == -1 )
+         {
+             return 1;
+         }
+        xv = -xv;
+    }
     //top curve (bounce off block of space), bottom of board
-    if (y>760 || y<118) yv = -yv;
+    if (y>760 || y<118) 
+    {
+        //Play effect
+         if( Mix_PlayChannel( -1, bounce, 0 ) == -1 )
+         {
+             return 1;
+         }
+        yv = -yv;
+    }
     //part of blocks
     if (x>270 && y > 888 || x<190 && y > 888) 
     {
+        //Play effect
+         if( Mix_PlayChannel( -1, bounce, 0 ) == -1 )
+         {
+             return 1;
+         }
         xv=-xv;
     	yv=-yv;
     }
     //part of blocks
     if (x>270 && y > 724 || x<190 && y > 724) 
     {
+        //Play effect
+         if( Mix_PlayChannel( -1, bounce, 0 ) == -1 )
+         {
+             return 1;
+         }
     	xv=-xv;
-    	
-}
+    }
 
 void Ball::draw(SDL_Surface* screen){
     SDL_Rect offset;
